b47ff275087b65ab960cf43ec2d5d1c48dcc5a06
Shreyaaaa07 <shreyabhosale711@gmail.com>
2026-08-15 18:26:10 +0530

    added graphs in plotting file

--- Shreya/Data Visualization using Matplotlib and Seaborn/matpotlib_denmo.py
+++ Shreya/Data Visualization using Matplotlib and Seaborn/matpotlib_denmo.py
@@ -1,1 +1,7 @@
-import matpotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+plt.plot(years, runs)
+plt.xlabel("Year")
+plt.ylabe("Runs Scored")
+plt.title("Sachin Tendulkar's Yearly Runs")
+plt.show()
